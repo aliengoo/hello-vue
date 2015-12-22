6626cd0ccd2bdfe31dc5806de1609192762746e6
Alien Goo <glenn.android@gmail.com>
2015-12-22 16:40:09 +0000

    wip

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -4,19 +4,15 @@ import Q from 'q';
 import pageHeader from '../../components/page-header';
 import row from '../../components/row';
 import container from '../../components/container';
-import navbar from '../../components/navbar';
 import authenticationApi from '../../common/AuthenticationApi';
 
 export default {
-  components: {pageHeader, row, container, navbar},
+  components: {pageHeader, row, container},
   template: `
-      <div>
-        <navbar route-name="home"></navbar>
-        <container>
-          <row>
-            <page-header>Home</page-header>
-          </row>
-        </container>
-      </div>
+      <container>
+        <row>
+          <page-header>Home</page-header>
+        </row>
+      </container>
     `
 };
